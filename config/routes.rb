faba2714852a1873223416d68d43336650a6f2d7
adityasuwandi <markusaditya01@gmail.com>
2018-03-21 01:24:49 +0700

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :binarians
   get 'welcome/index'
-
+ #wasasasas
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
