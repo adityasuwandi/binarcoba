5644df578ea2fefc36b326141dadfeae4e138d6c
adityasuwandi <markusaditya01@gmail.com>
2018-04-02 22:46:15 +0700

    api test

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
 
  # Get login token from Knock
  post   'user_token'      => 'user_token#create'
-
+ resources :users
  # User actions
  get    '/users'          => 'users#index'
  get    '/users/current'  => 'users#current'
