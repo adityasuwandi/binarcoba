3b225a3de5167621c5ad1033cfdfc8c4ccc78526
adityasuwandi <markusaditya01@gmail.com>
2018-04-02 22:49:39 +0700

    api test

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,6 @@ Rails.application.routes.draw do
  post   '/users/create'   => 'users#create'
  patch  '/user/:id'       => 'users#update'
  delete '/user/:id'       => 'users#destroy'
- #wasasasas
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
